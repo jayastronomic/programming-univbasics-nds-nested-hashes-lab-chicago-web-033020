a80c85b7130fda4fca3c76dd7ce31334a7a2c0a6
Julian Antonio Smith <juliansmith94@hotmail.com>
2020-03-25 05:48:25 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,6 +18,7 @@ def assembled_aoh
      { name:  "Kate Gompert", occupation: "None" },
      { name:  "Bruce Green", occupation: "Fan of Mildred" } 
     ]
+    media_staff_2
 end
 
 def literal_aoh
